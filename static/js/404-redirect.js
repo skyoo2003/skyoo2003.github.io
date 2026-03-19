d8c439c56b2914ffe7470db681515cbf67c1d0f6
Sung-Kyu Yoo <skyoo2003@gmail.com>
2026-03-19 22:21:05 +0900

    fix: extract redirect JS to shared file, use ES5-compatible code

---
+++ static/js/404-redirect.js
@@ -0,0 +1,27 @@
+(function() {
+  var path = window.location.pathname;
+  var search = window.location.search;
+  var hash = window.location.hash;
+  
+  if (path.match(/^\/(ko|en)(\/|$)/)) {
+    return;
+  }
+  
+  var patterns = ['posts', 'about', 'tags', 'search'];
+  var shouldRedirect = patterns.some(function(p) {
+    return path === '/' + p || path === '/' + p + '/' || path.indexOf('/' + p + '/') === 0;
+  });
+  
+  if (!shouldRedirect) {
+    return;
+  }
+  
+  if (path.charAt(path.length - 1) !== '/') {
+    path = path + '/';
+  }
+  
+  var lang = (navigator.language || navigator.userLanguage || '').toLowerCase();
+  var prefix = lang.indexOf('ko') === 0 ? '/ko' : '/en';
+  
+  window.location.replace(prefix + path + search + hash);
+})();
